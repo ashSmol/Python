d1e679d4c1551e19dd7f76503c1fe9cc9f351784
ashSmol <ashiryaev84@gmail.com>
2020-10-23 20:50:01 +0300

    Added 2st tasks

--- task-2.py
+++ task-2.py
@@ -3,12 +3,12 @@ class Road:
     _width = 20
 
     def __init__(self):
-        Road._length = int(input("Введите длину дорого: "))
-        Road._width = int(input("Введите ширину дороги: "))
+        Road._length = int(input("Введите длину дороги в метрах: "))
+        Road._width = int(input("Введите ширину дороги в метрах: "))
 
-    def _calculate(self):
+    def calculate(self):
         return Road._length * Road._width * 25 * 5 / 1000
 
 
 r1 = Road()
-print(f"Для постройки такой дороги потребуется {r1._calculate()} тонн асфальта")
+print(f"Для постройки такой дороги потребуется {r1.calculate()} тонн асфальта")

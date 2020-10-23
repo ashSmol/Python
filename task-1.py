ab3b45c2c055a81c690a9b6d988fcb26a5370071
ashSmol <ashiryaev84@gmail.com>
2020-10-23 20:20:16 +0300

    Added 1st tasks

--- task-1.py
+++ task-1.py
@@ -5,14 +5,14 @@ from time import sleep
 class TrafficLight:
     __color = ''
 
+    def running(self):
+        colors_order = [("red", 7), ("yellow", 3), ("green", 7), ("yellow", 3)]
 
-def running(self):
-    colors_order = {"red": 7, "yellow": 3, "green": 7, "yellow": 3}
-    for elem in cycle(colors_order.items()):
-        TrafficLight.__color = elem[0]
-        print(f"{TrafficLight.__color}")
-        sleep(elem[1])
+        for elem in cycle(colors_order):
+            TrafficLight.__color = elem[0]
+            print(f"{TrafficLight.__color}")
+            sleep(elem[1])
 
 
-tl = TrafficLight
+tl = TrafficLight()
 tl.running()

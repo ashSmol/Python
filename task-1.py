0241a6d73e61fddb00212cceb876101370afa4d6
ashSmol <ashiryaev84@gmail.com>
2020-10-28 00:13:18 +0300

    Added half task-1.py

---
+++ task-1.py
@@ -0,0 +1,29 @@
+from random import randint
+
+
+class Matrix:
+
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def __str__(self):
+        str_matrix = ''
+        for row in self.matrix:
+            for el in row:
+                str_matrix += str(el) + '\t'
+            str_matrix += '\n'
+        return str_matrix
+
+    def __add__(self, other):
+        new_matrix = [[el_self + el_other for el_self, el_other in zip(row_self, row_other)] for row_self, row_other in
+                      zip(self.matrix, other.matrix)]
+        return Matrix(new_matrix)
+
+
+m_1 = Matrix([[randint(-100, 101) for _ in range(3)] for _ in range(3)])
+m_2 = Matrix([[randint(-100, 101) for _ in range(3)] for _ in range(3)])
+print(m_1)
+print()
+print(m_2)
+print()
+print(m_1 + m_2)

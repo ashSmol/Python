c19342138ecaa03f3b20a5c4771a5c62f0585a82
ashSmol <ashiryaev84@gmail.com>
2020-10-28 22:00:06 +0300

    Added task-2.py, task-3.py.

--- task-1.py
+++ task-1.py
@@ -15,15 +15,19 @@ class Matrix:
         return str_matrix
 
     def __add__(self, other):
-        new_matrix = [[el_self + el_other for el_self, el_other in zip(row_self, row_other)] for row_self, row_other in
-                      zip(self.matrix, other.matrix)]
+        if len(self.matrix) == len(other.matrix) & len(self.matrix[0]) == len(other.matrix[0]):
+            new_matrix = [[el_self + el_other for el_self, el_other in zip(row_self, row_other)] for row_self, row_other
+                          in
+                          zip(self.matrix, other.matrix)]
+        else:
+            return str("Матрицы разноразмерные!!! Сложение невозможно!!!")
         return Matrix(new_matrix)
 
 
 m_1 = Matrix([[randint(-100, 101) for _ in range(3)] for _ in range(3)])
 m_2 = Matrix([[randint(-100, 101) for _ in range(3)] for _ in range(3)])
 print(m_1)
-print()
+
 print(m_2)
-print()
+
 print(m_1 + m_2)

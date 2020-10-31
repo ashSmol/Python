2ef44b151a3c258f25344341f264f88b729ea0ad
ashSmol <ashiryaev84@gmail.com>
2020-10-31 22:22:51 +0300

    task-1.py

--- task-1.py
+++ task-1.py
@@ -1,26 +1,37 @@
 class Date:
+    day = None
+    month = None
+    year = None
+
     def __init__(self, str_date):
         self.str_date = str_date
+        Date.make_int(str_date)
 
     @classmethod
     def make_int(cls, str_date):
-        day = str_date.split('-')[0]
-        month = str_date.split('-')[1]
-        year = str_date.split('-')[2]
-        return int(day), int(month), int(year)
+        try:
+            lst_date = str_date.split('-')
+            cls.day = int(lst_date[0])
+            cls.month = int(lst_date[1])
+            cls.year = int(lst_date[2])
+        except ValueError as err:
+            print(err)
 
     def __str__(self):
-        return self.str_date
+        Date.make_int(self.str_date)
+        return f'{self.day}-{self.month}-{self.year}'
 
     @staticmethod
-    def validate(day, month, year):
-        if (0 < day <= 31) & (0 < month <= 12) & (0 < year <= 9999):
-            print('Дата корректна')
-        else:
-            print("Дата некорректна")
+    def validate(date):
+        try:
+            if (0 < date.day <= 31) & (0 < date.month <= 12) & (0 < date.year <= 9999):
+                print('Дата корректна')
+            else:
+                print("Дата некорректна")
+        except TypeError as err:
+            print(err)
 
 
-d = Date('11-12-2020')
+d = Date('01-12-2020')
 print(d)
-print(Date.make_int('11-12-2200'))
-d.validate(*Date.make_int('11-12-2200'))
+d.validate(d)

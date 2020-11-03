e64dae9785f5fe24c0fdcf65832e958a4606f975
ashSmol <ashiryaev84@gmail.com>
2020-11-03 23:34:03 +0300

    a

--- task-4-5-6-v2.py
+++ task-4-5-6-v2.py
@@ -1,39 +1,54 @@
 item_reference = {}
 
 
+class Company:
+    def __init__(self, depts_list):
+        self.depts = {}
+        for el in depts_list:
+            self.depts.update({len(self.depts) + 1: el.name})
+
+    def __str__(self):
+        result = ''
+        for el in self.depts:
+            result += f'{el}. {self.depts.get(el)}\n'
+        return result
+
+
 class NegativeAmountException(Exception):
     def __init__(self, err_text):
         self.err_text = err_text
 
 
 def register_item(item):
-    item_reference[len(item_reference)] = item
-
-
-def move_item(from_dep_1, to_dep_2, item_id, quantity=1):
-    try:
-        if from_dep_1.items_quantities[item_id] - quantity < 0:
-            raise NegativeAmountException(f'Невозможно переместить оборудования больше чем есть в отделе')
-        from_dep_1.items_quantities[item_id] = from_dep_1.items_quantities[item_id] - quantity
-        to_dep_2.items_quantities[item_id] = to_dep_2.items_quantities[item_id] + quantity
-    except NegativeAmountException as err:
-        print(err)
+    item_reference[len(item_reference) + 1] = item
 
 
 class CompanyDepartment:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.items_quantities = {key_: 0 for key_ in item_reference.keys()}
 
     def __str__(self):
         result = ''
         for el in self.items_quantities:
             if self.items_quantities.get(el) > 0:
-                result += f'{item_reference.get(el)} - {self.items_quantities[el]} шт \n'
+                result += f'{el}. {item_reference.get(el).__class__.__name__} {item_reference.get(el)} - {self.items_quantities[el]} шт \n'
         return result
+
+    @staticmethod
+    def move_item(from_dep_1, to_dep_2, item_id, quantity=1):
+        try:
+            if from_dep_1.items_quantities[item_id] - quantity < 0:
+                raise NegativeAmountException(f'Невозможно переместить оборудования больше чем есть в отделе')
+            from_dep_1.items_quantities[item_id] = from_dep_1.items_quantities[item_id] - quantity
+            to_dep_2.items_quantities[item_id] = to_dep_2.items_quantities[item_id] + quantity
+        except NegativeAmountException as err:
+            print(err)
 
 
 class Warehouse(CompanyDepartment):
     def __init__(self):
+        self.name = 'Склад'
         self.items_quantities = {key_: 5 for key_ in item_reference.keys()}
 
 
@@ -82,13 +97,23 @@ register_item(Copier('Xerox', 'WorkCentre 3025V_BI', True))
 register_item(Copier('Pantum ', 'M6500', False))
 register_item(Copier('brother', 'copier555', True))
 
-dep_1 = CompanyDepartment()
-dep_2 = CompanyDepartment()
+counting_dept = CompanyDepartment('Бухгалтерия')
+h_r_dept = CompanyDepartment('Отдел Кадров')
+admin_dept = CompanyDepartment('Администрация')
+dev_dep = CompanyDepartment('Разработка')
+design_dep = CompanyDepartment('Дизайн')
 wh = Warehouse()
 
-move_item(wh, dep_1, 5, 3)
-move_item(wh, dep_1, 6, 4)
+depts_list = [counting_dept, h_r_dept, admin_dept, dev_dep, design_dep]
 
-move_item(dep_1, dep_2, 5)
 
-print(dep_2)
+my_company = Company(depts_list)
+user_command = ''
+
+#d = {}
+#print({len(d) + 1: el for el in 'asdfg'})
+
+while user_command != 'выход':
+    print('*' * 50 + '   Меню   ' + '*' * 50)
+    print('введите номер оборудования для перемещения: ')
+    item_id = input()
